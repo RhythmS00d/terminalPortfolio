050da1fa413be16d409c5ceb5ce5bf095916d226
RhythmS00d <rhythmsood1@gmail.com>
2024-08-06 16:45:27 +1000

    added new commands

--- src/utils/triggerCommand.jsx
+++ src/utils/triggerCommand.jsx
@@ -11,9 +11,13 @@ const defaultCommands = [
   "projects",
   "github",
   "hobbies",
+  "help",
+  "whoami",
+  "welcome",
 ];
 
 const commandActions = {
+  help,
   linkedin,
   email,
   education,
@@ -23,58 +27,208 @@ const commandActions = {
   projects,
   github,
   hobbies,
+  whoami,
+  welcome,
 };
 
 export function triggerCommand(command, setCommandList) {
-  const cmd = command.substring(1);
-
   function updateList(item) {
-    setCommandList((prev) => [...prev, item]);
+    const updatedItem = (
+      <div>
+        <p>
+          <span className="visitor">visitor</span>@
+          <span className="location">example</span>:-$ {command}
+        </p>
+        {item}
+      </div>
+    );
+    setCommandList((prev) => [...prev, updatedItem]);
   }
 
-  if (defaultCommands.includes(cmd)) {
-    const action = commandActions[cmd];
+  if (command === "clear") {
+    setCommandList([]);
+    return;
+  }
+
+  if (defaultCommands.includes(command)) {
+    const action = commandActions[command];
     if (action) action(updateList);
   } else {
-    setCommandList((prev) => [
-      ...prev,
-      `Error: command *${cmd}* does not exist! Need help? Enter /help`,
-    ]);
+    updateList(
+      `Error: command *${command}* does not exist! Need help? Enter help.`
+    );
   }
+}
+
+function welcome(updateList) {
+  const element = (
+    <div className="welcome-container">
+      <p>Welcome to my terminal based portfolio. (Version 1.0.0)</p>
+      <p>----</p>
+      <p>
+        This project's source code can be found in this{" "}
+        <a href="">Github Repo</a>.
+      </p>
+      <p>----</p>
+      <p>For a list of available commands, type 'help'.</p>
+    </div>
+  );
+
+  updateList(element);
+}
+
+function help(updateList) {
+  const commands = [
+    {
+      command: "clear",
+      message: "Clears the terminal.",
+    },
+    {
+      command: "help",
+      message: "Returns all available commands.",
+    },
+    {
+      command: "whoami",
+      message: "Use it to know a bit about me.",
+    },
+    {
+      command: "linkedin",
+      message: "Gives my LinkedIn profile link.",
+    },
+    {
+      command: "email",
+      message: "Gives my email address.",
+    },
+    {
+      command: "github",
+      message: "Gives my Github profile link.",
+    },
+    {
+      command: "education",
+      message: "Retrieves my education background.",
+    },
+    {
+      command: "skills",
+      message: "Retrieves my professional skills.",
+    },
+    {
+      command: "projects",
+      message: "Gives a list of projects I built along with their source code.",
+    },
+    {
+      command: "experience",
+      message: "Retrieves my technical and retail experience.",
+    },
+    {
+      command: "certificates",
+      message: "Retrieves my professional certificates.",
+    },
+    {
+      command: "hobbies",
+      message: "Retrieves my hobbies.",
+    },
+  ];
+  const element = (
+    <div>
+      <ul>
+        <li>
+          Each command starts with a '/', which is already fixed in the input
+          field.
+        </li>
+        <li>
+          Commands can be used by clicking the submit button or by pressing
+          enter.
+        </li>
+        <li>Each incorrect entered command will give an error.</li>
+      </ul>
+      <br />
+      <ul>
+        {commands.map((command, index) => {
+          return (
+            <li key={index + command}>
+              <strong className="command-text">{command.command}</strong>:{" "}
+              <i>{command.message}</i>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+  updateList(element);
+}
+
+function whoami(updateList) {
+  const element = (
+    <div>
+      <p>
+        My name is <strong>Rhythm Sood</strong>. I am a recent graduate Software
+        Engineer from Macquarie University.
+      </p>
+      <p>
+        Driven software developer with expertise in blockchain, Java, Python,
+        and frontend technologies. Seeking a challeng- ing role to leverage my
+        skills in developing innovative solutions. Committed to excellence, I
+        aim to contribute to projects that push boundaries and align with my
+        passion for quality code, collaborative teamwork, and continuous
+        improvement.
+      </p>
+    </div>
+  );
+
+  updateList(element);
 }
 
 function linkedin(updateList) {
   const element = (
-    <a target="_blank" href="https://www.linkedin.com/in/rhythm-sood/">
-      LinkedIn
-    </a>
+    <p>
+      Find me on:
+      <a
+        className="social-links"
+        target="_blank"
+        href="https://www.linkedin.com/in/rhythm-sood/"
+      >
+        LinkedIn
+      </a>
+    </p>
   );
   updateList(element);
 }
 
 function github(updateList) {
   const element = (
-    <a target="_blank" href="https://github.com/RhythmS00d">
-      Github
-    </a>
+    <p>
+      Find my coding projects on:
+      <a
+        className="social-links"
+        target="_blank"
+        href="https://github.com/RhythmS00d"
+      >
+        Github
+      </a>
+    </p>
   );
   updateList(element);
 }
 
 function email(updateList) {
   const element = (
-    <a href="mailto:rhythmsood1@gmail.com">rhythmsood1@gmail.com</a>
+    <p>
+      Contact me via:
+      <a className="social-links" href="mailto:rhythmsood1@gmail.com">
+        rhythmsood1@gmail.com
+      </a>
+    </p>
   );
   updateList(element);
 }
 
 function experience(updateList) {
   const element = (
-    <section>
+    <div>
       <div>
         <strong>Retail Assistant</strong> at <strong>7 Eleven</strong>,{" "}
         <i>June 2021 - Present</i>
-        <ul>
+        <ol>
           <li>
             Provided excellent customer service, resulting in a 10% increase in
             customer satisfaction scores.
@@ -91,12 +245,13 @@ function experience(updateList) {
             Handled cash transactions and maintained accurate cash register
             balances.
           </li>
-        </ul>
+        </ol>
       </div>
+      <br />
       <div>
         <strong>Software Developer Intern</strong> at <strong>IBM</strong>,{" "}
         <i>July 2022 - December 2022</i>
-        <ul>
+        <ol>
           <li>
             Engineered a responsive frontend interface for a healthcare
             blockchain application, achieving a 15% improvement in user
@@ -116,9 +271,9 @@ function experience(updateList) {
             healthcare data protection by achieving a 25% reduction in security
             vulnerabilities.
           </li>
-        </ul>
+        </ol>
       </div>
-    </section>
+    </div>
   );
 
   updateList(element);
@@ -126,12 +281,12 @@ function experience(updateList) {
 
 function certificates(updateList) {
   const element = (
-    <section>
+    <div>
       <div>
         <strong>Kreativstorm Frontend Trainee</strong>
         <p>
           Understanding of how to use Js, HTML, and CSS. Built several web based
-          projects for great web development knowl- edge. (
+          projects for great web development knowledge. (
           <a
             href="https://api.credsverse.com/v1/qr/3ad1649a-4e03-4f57-b03c-6ba5efef167b"
             target="_blank"
@@ -155,7 +310,7 @@ function certificates(updateList) {
           )
         </p>
       </div>
-    </section>
+    </div>
   );
 
   updateList(element);
@@ -163,7 +318,7 @@ function certificates(updateList) {
 
 function projects(updateList) {
   const element = (
-    <section>
+    <div>
       <p>
         <strong>Wordle Clone</strong>: A react app made with MobX state
         management. Implemented high speed rendering and fun to use user
@@ -204,7 +359,7 @@ function projects(updateList) {
         </a>
         )
       </p>
-    </section>
+    </div>
   );
 
   updateList(element);
@@ -213,14 +368,11 @@ function projects(updateList) {
 function education(updateList) {
   const element = (
     <div>
+      <p>Bachelor of Information Technology, Macquarie University</p>
       <p>
-        <strong>Bachelor of Information Technology</strong>, Macquarie
-        University
-      </p>
-      <p>
-        Relevant Coursework: Mobile App Development, Full stack development,
-        Java, Object Oriented Programming and Programming languages with Scala,
-        Python.
+        <strong>Relevant Coursework</strong>: Mobile App Development, Full stack
+        development, Java, Object Oriented Programming and Programming languages
+        with Scala, Python.
       </p>
     </div>
   );
@@ -230,7 +382,7 @@ function education(updateList) {
 
 function skills(updateList) {
   const element = (
-    <section>
+    <div>
       <div>
         <label htmlFor="">
           <strong>Technical Skills</strong>
@@ -245,7 +397,7 @@ function skills(updateList) {
         : Communication, Problem Solving, Adaptability, Multitasking and
         Teamwork
       </div>
-    </section>
+    </div>
   );
 
   updateList(element);
