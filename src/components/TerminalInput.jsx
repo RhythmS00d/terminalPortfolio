05754356fe86f54fd40bc28df4c4114a1de35ece
RhythmS00d <rhythmsood1@gmail.com>
2024-08-06 16:46:15 +1000

    removed modal code, fixed a minor bug of case sensitive commands or key press

--- src/components/TerminalInput.jsx
+++ src/components/TerminalInput.jsx
@@ -3,39 +3,37 @@ import { useState, useRef } from "react";
 
 import { triggerCommand } from "../utils/triggerCommand";
 
-export const TerminalInput = ({ setCommandList, setModal, modal }) => {
-  const [inputValue, setInputValue] = useState("/");
+export const TerminalInput = ({ setCommandList }) => {
+  const [inputValue, setInputValue] = useState("");
   const inputRef = useRef();
 
-  const handleKeyDown = () => {
-    if(inputValue.length < 2) return;
+  const handleKeyDown = (key) => {
+    if (inputValue.length < 1) return;
 
-    if(inputValue.substring(1) === 'help') {
-      setModal(true);
-      inputRef.current.blur();
-      return;
+    if (key.toLowerCase() === "enter" || key.toLowerCase() === "return") {
+      triggerCommand(inputValue, setCommandList);
+      setInputValue("");
+      inputRef.current.focus();
+      inputRef.current.scrollIntoView();
     }
-    triggerCommand(inputValue, setCommandList)
-    setInputValue("/");
-    inputRef.current.focus();
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Enter command here....."
-        value={inputValue.toLowerCase()}
-        onChange={(e) =>
-          e.target.value.length < 1 ? "/" : setInputValue(e.target.value)
-        }
-        autoFocus={!modal}
-        ref={inputRef}
-        onKeyDown={(e) => (e.key === "Enter" ? handleKeyDown() : () => {})}
-      />
-      <button type="button" onClick={handleKeyDown}>
-        go
-      </button>
-    </div>
+    <section className="input-container">
+      <label htmlFor="terminal-input">
+        <span className="visitor">visitor</span>@
+        <span className="location">example</span>:-$
+        <input
+          id="terminal-input"
+          type="text"
+          placeholder="Enter command here....."
+          value={inputValue.toLowerCase()}
+          onChange={(e) => setInputValue(e.target.value.toLowerCase())}
+          autoFocus
+          ref={inputRef}
+          onKeyDown={(e) => handleKeyDown(e.key)}
+        />
+      </label>
+    </section>
   );
 };
