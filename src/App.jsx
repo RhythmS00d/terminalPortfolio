e2d984d91a29285eae3de2a4e22a13151ed3b8d2
RhythmS00d <rhythmsood1@gmail.com>
2024-08-06 16:46:42 +1000

    unnecessary imports, added welcome initial command

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,42 @@ import { useState } from "react";
 
 // components imports
 import { CommandList } from "./components/CommandList";
-import { CustomButton } from "./components/CustomButton";
-import { HelpModal } from "./components/HelpModal";
 import { TerminalInput } from "./components/TerminalInput";
+import { Resume } from "./components/Resume";
+import { Header } from "./components/Header";
 
-import "./App.css"
+import "./App.css";
 
 function App() {
-  const [commandList, setCommandList] = useState([]);
-  const [modal, setModal] = useState(false);
+  const [commandList, setCommandList] = useState([
+    <div className="welcome-container">
+      <p>
+        <span className="visitor">visitor</span>@
+        <span className="location">example</span>:-$ welcome
+      </p>
+      <p>Welcome to my terminal based portfolio. (Version 1.0.0)</p>
+      <p>----</p>
+      <p>
+        This project's source code can be found in this{" "}
+        <a href="">Github Repo</a>.
+      </p>
+      <p>----</p>
+      <p>For a list of available commands, type 'help'.</p>
+    </div>,
+  ]);
+  const [currentPage, setCurrentPage] = useState("Terminal");
 
   return (
     <main>
-      <HelpModal modal={modal} setModal={setModal}/>
-      <CommandList commandList={commandList} />
-      <TerminalInput setCommandList={setCommandList} setModal={setModal} modal={modal}/>
+      <Header setCurrentPage={setCurrentPage} />
+      {currentPage === "Terminal" ? (
+        <div className="content">
+          <CommandList commandList={commandList} />
+          <TerminalInput setCommandList={setCommandList} />
+        </div>
+      ) : (
+        <Resume />
+      )}
     </main>
   );
 }
